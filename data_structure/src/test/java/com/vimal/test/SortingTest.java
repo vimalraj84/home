2ff98042235eee1436110aea8e43778d74174306
Karunakaran <vkarunakaran@deloitte.com>
2019-03-26 16:48:45 -0400

    Tree Implementation

--- data_structure/src/test/java/com/vimal/test/SortingTest.java
+++ data_structure/src/test/java/com/vimal/test/SortingTest.java
@@ -3,14 +3,23 @@ package com.vimal.test;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.vimal.bo.emp.EmpComparator;
 import com.vimal.bo.emp.Employee;
 import com.vimal.bo.utils.StubGenerator;
 
 public class SortingTest {
 	
-	public static void main(String[] a) {
-		List<Employee> emps = StubGenerator.getEmployeeList();
+	List<Employee> emps = null;
+	@BeforeEach
+	void setUp() throws Exception {
+		emps = StubGenerator.getEmployeeList();
+	}
+	
+	@Test
+	final void testSort() throws Exception {
 		System.out.println("BEFORE SORTING : ");
 		emps.forEach(e -> System.out.println(e.getName().getfName()+" "+e.getName().getlName()+" "+e.getAge()));
 		
@@ -18,4 +27,5 @@ public class SortingTest {
 		System.out.println("\n\nAFTER SORTING : ");
 		emps.forEach(e -> System.out.println(e.getName().getfName()+" "+e.getName().getlName()+" "+e.getAge()));
 	}
+	
 }
